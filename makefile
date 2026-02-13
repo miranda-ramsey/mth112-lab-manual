6bec3e976175aaed18a8ba3c7cfda85187ffd089
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2026-02-13 13:42:58 -0800

    basic structure

---
+++ makefile
@@ -0,0 +1,7 @@
+web:
+	python3 -m pretext build web
+	python3 -m pretext view web
+	
+pdf:
+	python3 -m pretext build pdf
+	python3 -m pretext view pdf
---
+++ output/web/lunr-pretext-search-index.js
@@ -0,0 +1,185 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "preface",
+  "level": "1",
+  "url": "preface.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Preface",
+  "body": " Preface  This is the second part of the two part lab manual for MTH111 and MTH112 at Portland Community College. The first chapter is \"Chapter 5\" to align with the first portion of the lab manual. This lab manual was designed with the intent of being used in the following manner. In each section:   Students will complete the Preparation Exercises before receiving instruction on the content. Some instructors will have students do this before coming to class, while others might do it as a warmup in class.    There will be some instructor-led presentation of the content. This may be a formal class lecture, a discovery activity, a video lecture, or something else.    Students will then engage in Practice Exercises in a group setting to reinforce their initial understanding of the foundational concepts.    An instructor will then assign one or more group activities. Because many of these activities are webbased and instructors can choose to use different activities, we have not included any of the possible activities in this document.    The Definitions are meant to provide a single location for all definitions and some key concepts. Students can use this as a resource after having covered the topics in class.    Students will complete some or all of the Exit Exercises, as decided by their instructor, to summarize their understanding of key concepts.    In general, not every section of this lab manual will take the same amount of class time. Some sectionsmight be half-day topics, while others could be two-day topics.    Each instructor will identify for their class which exercises will be submitted as part of the course grade.     Course Resource Links:   Algebra and Trigonometry 2e       MTH 112 Supplement        "
+},
+{
+  "id": "sec-angles",
+  "level": "1",
+  "url": "sec-angles.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Angles",
+  "body": " Angles   In this section, we'll    "
+},
+{
+  "id": "sec-right-angle-trig",
+  "level": "1",
+  "url": "sec-right-angle-trig.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Right Angle Trigonometry",
+  "body": " Right Angle Trigonometry   In this section, we'll    "
+},
+{
+  "id": "sec-unit-circle",
+  "level": "1",
+  "url": "sec-unit-circle.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "The Unit Circle",
+  "body": " The Unit Circle   In this section, we'll    "
+},
+{
+  "id": "sec-other-trig-functions",
+  "level": "1",
+  "url": "sec-other-trig-functions.html",
+  "type": "Section",
+  "number": "5.4",
+  "title": "Other Trigonometric Functions",
+  "body": " Other Trigonometric Functions   In this section, we'll    "
+},
+{
+  "id": "sec-sine-cosine-graphs",
+  "level": "1",
+  "url": "sec-sine-cosine-graphs.html",
+  "type": "Section",
+  "number": "6.1",
+  "title": "Graphs of Sine and Cosine Functions",
+  "body": " Graphs of Sine and Cosine Functions   In this section, we'll    "
+},
+{
+  "id": "sec-other-graphs",
+  "level": "1",
+  "url": "sec-other-graphs.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Graphs of Other Trigonometric Functions",
+  "body": " Graphs of Other Trigonometric Functions   In this section, we'll    "
+},
+{
+  "id": "sec-inverse-trigonometry",
+  "level": "1",
+  "url": "sec-inverse-trigonometry.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Inverse Trigonometry",
+  "body": " Inverse Trigonometry   In this section, we'll    "
+},
+{
+  "id": "sec-summary",
+  "level": "1",
+  "url": "sec-summary.html",
+  "type": "Section",
+  "number": "7.1",
+  "title": "Trigonometric Identities Summary Sheet",
+  "body": " Trigonometric Identities Summary Sheet   In this section, we'll    "
+},
+{
+  "id": "sec-trig-identities",
+  "level": "1",
+  "url": "sec-trig-identities.html",
+  "type": "Section",
+  "number": "7.2",
+  "title": "Trigonometric Identities",
+  "body": " Trigonometric Identities   In this section, we'll    "
+},
+{
+  "id": "sec-sum-and-difference",
+  "level": "1",
+  "url": "sec-sum-and-difference.html",
+  "type": "Section",
+  "number": "7.3",
+  "title": "Sum And Difference",
+  "body": " Sum And Difference   In this section, we'll    "
+},
+{
+  "id": "sec-double-angle",
+  "level": "1",
+  "url": "sec-double-angle.html",
+  "type": "Section",
+  "number": "7.4",
+  "title": "Double Angle Identities",
+  "body": " Double Angle Identities   In this section, we'll    "
+},
+{
+  "id": "sec-sum-to-product",
+  "level": "1",
+  "url": "sec-sum-to-product.html",
+  "type": "Section",
+  "number": "7.5",
+  "title": "Sum-to-Product Identities",
+  "body": " Sum-to-Product Identities   In this section, we'll    "
+},
+{
+  "id": "sec-solving-equations",
+  "level": "1",
+  "url": "sec-solving-equations.html",
+  "type": "Section",
+  "number": "7.6",
+  "title": "Solving Trigonometric Equations",
+  "body": " Solving Trigonometric Equations   In this section, we'll    "
+},
+{
+  "id": "sec-polar-coordinates",
+  "level": "1",
+  "url": "sec-polar-coordinates.html",
+  "type": "Section",
+  "number": "8.1",
+  "title": "Polar Coordinates",
+  "body": " Polar Coordinates   In this section, we'll    "
+},
+{
+  "id": "sec-complex-numbers",
+  "level": "1",
+  "url": "sec-complex-numbers.html",
+  "type": "Section",
+  "number": "8.2",
+  "title": "Complex Numbers",
+  "body": " Complex Numbers   In this section, we'll    "
+},
+{
+  "id": "sec-vectors",
+  "level": "1",
+  "url": "sec-vectors.html",
+  "type": "Section",
+  "number": "8.3",
+  "title": "Vectors",
+  "body": " Vectors   In this section, we'll    "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})

0857f98592b3cb8dcec429a3742ea709568e2199
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2026-02-14 11:03:40 -0800

    add angles section outline

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -25,7 +25,178 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "5.1",
   "title": "Angles",
-  "body": " Angles   In this section, we'll    "
+  "body": " Angles   In this section, we'll draw angles in standard position, convert between degrees and radians and degrees-minutes-seconds, find coterminal angles, and find the length of a circular arc.    Textbook Reference  This relates to content in 7.1 of Algebra and Trigonometry 2e .    Preparation Exercises    Answer the following without using a calculator. These questions are intended to check the prerequisite skills needed to complete the rest of the lab.     Solve the equation for .      Simplify the expression without a calculator. Reduce to simplest form.      In terms of time, how many minutes total are in 3 hours and 53 minutes?      Draw a line segment from the point to the point . Find the distance between those two points using the Pythagorean Theorem. Leave your answer in exact form.        Practice Exercises    Find an angle that is coterminal to -40 and is between 360 and 720       Match each angle in the first group with its coterminal angle in the second group.     30  270  -220  -60  0  150      720  -90  390  510  -420  500         Convert the angles.     Convert 28 to radians. Leave your answer as a fraction in terms of .      Convert radians to degrees. Round to 6 digits behind the decimal place.      Convert 14 20 36 to a decimal measurement of degrees.      Convert 76.85 to degrees-minutes-seconds.       Use the formula to find the missing values.     Find the arc length of on a circle of radius 6 with an interior angle of .      Find the interior angle of an arc length on a circle of radius 12 with an arc length of .      Find the arc length of on a circle of radius with an interior angle of 20        Definitions   Angle   An angle is formed when two lines intersect at a point. We can measure the “size” of this angle in degrees or radians.     Ray   A ray is a portion of a line that starts at a point and extends infinitely in one direction.     Degree   A degree is an angle measurement equal to of a full rotation.     Right Angle   A right angle is an angle of 90 .     Radian   A radian is a unit of measurement of angle size, and 1 radian is defined to be the angle that is made when the arc length for that angle on a circle of radius 1 is equal to 1. Check out this Desmos link and click the \"play\" button on the slider for a visual representation of this concept.    Standard Angles    A circle with common angles indicated in radians:   0     30     45     60     90     120     135     150     180     210     225     240     270     300     315     330            Initial and Terminal sides   An angle is created by imagining two rays originating from the same point: the initial side of the angle is stationary and the terminal side is rotated around until the desired angle is created. Angles measured from initial to terminal side in a counterclockwise direction are positive. Angles measured from initial to terminal side clockwise are negative.     Standard Position   An angle is said to be in standard position if the initial and terminal sides of the angle meet at the origin and the initial side of the angle is along the positive x-axis.     Coterminal   Two angles are coterminal if their initial and terminal sides align, but they differ by an integer number of full rotations. In standard position, this means that the two angles are “in the same place” are different by some integer multiple of 360 (or radians).     Arc length   An arc length is a distance along a part of the circumference of a circle. A formula for arc length is , where stands for arc length along a circle of radius , with an interior angle centered at the center of the circle.    Coterminal angles and    An angle drawn in standard position is indicated in two ways. Beginning with an initial side along the positive -axis and ending with a terminal side in the second quadrant, the angle in the counter- clockwise direction is labeled and the angle in the clockwise direction is labeled . The segment of the circumference of the circle between the initial and terminal sides of is the arc subtended by , and the length measure of that arc is the arc length for angle .         Exit Exercises    Explain why -90 is not the same as 270 .      Convert 34 29 24 to decimal degrees.      Convert the angle 144 to radians. Write your answer in exact form as a fraction with in it.      How big of an arc length will an angle of radians create on a circle of radius 6? Draw this angle and arc length on the circle of radius 6 provided.         On a scale of 1 5, how are you feeling with the concepts related to the graphical behaviors of functions?    --> "
+},
+{
+  "id": "sec-angles-4-2",
+  "level": "2",
+  "url": "sec-angles.html#sec-angles-4-2",
+  "type": "Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Answer the following without using a calculator. These questions are intended to check the prerequisite skills needed to complete the rest of the lab.     Solve the equation for .      Simplify the expression without a calculator. Reduce to simplest form.      In terms of time, how many minutes total are in 3 hours and 53 minutes?      Draw a line segment from the point to the point . Find the distance between those two points using the Pythagorean Theorem. Leave your answer in exact form.     "
+},
+{
+  "id": "sec-angles-5-2",
+  "level": "2",
+  "url": "sec-angles.html#sec-angles-5-2",
+  "type": "Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Find an angle that is coterminal to -40 and is between 360 and 720    "
+},
+{
+  "id": "sec-angles-5-3",
+  "level": "2",
+  "url": "sec-angles.html#sec-angles-5-3",
+  "type": "Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Match each angle in the first group with its coterminal angle in the second group.     30  270  -220  -60  0  150      720  -90  390  510  -420  500      "
+},
+{
+  "id": "sec-angles-5-4",
+  "level": "2",
+  "url": "sec-angles.html#sec-angles-5-4",
+  "type": "Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Convert the angles.     Convert 28 to radians. Leave your answer as a fraction in terms of .      Convert radians to degrees. Round to 6 digits behind the decimal place.      Convert 14 20 36 to a decimal measurement of degrees.      Convert 76.85 to degrees-minutes-seconds.    "
+},
+{
+  "id": "sec-angles-5-5",
+  "level": "2",
+  "url": "sec-angles.html#sec-angles-5-5",
+  "type": "Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Use the formula to find the missing values.     Find the arc length of on a circle of radius 6 with an interior angle of .      Find the interior angle of an arc length on a circle of radius 12 with an arc length of .      Find the arc length of on a circle of radius with an interior angle of 20     "
+},
+{
+  "id": "definitions-angles-2",
+  "level": "2",
+  "url": "sec-angles.html#definitions-angles-2",
+  "type": "Definition",
+  "number": "5.1.1",
+  "title": "Angle.",
+  "body": " Angle   An angle is formed when two lines intersect at a point. We can measure the “size” of this angle in degrees or radians.   "
+},
+{
+  "id": "definitions-angles-3",
+  "level": "2",
+  "url": "sec-angles.html#definitions-angles-3",
+  "type": "Definition",
+  "number": "5.1.2",
+  "title": "Ray.",
+  "body": " Ray   A ray is a portion of a line that starts at a point and extends infinitely in one direction.   "
+},
+{
+  "id": "definitions-angles-4",
+  "level": "2",
+  "url": "sec-angles.html#definitions-angles-4",
+  "type": "Definition",
+  "number": "5.1.3",
+  "title": "Degree.",
+  "body": " Degree   A degree is an angle measurement equal to of a full rotation.   "
+},
+{
+  "id": "definitions-angles-5",
+  "level": "2",
+  "url": "sec-angles.html#definitions-angles-5",
+  "type": "Definition",
+  "number": "5.1.4",
+  "title": "Right Angle.",
+  "body": " Right Angle   A right angle is an angle of 90 .   "
+},
+{
+  "id": "definitions-angles-6",
+  "level": "2",
+  "url": "sec-angles.html#definitions-angles-6",
+  "type": "Definition",
+  "number": "5.1.5",
+  "title": "Radian.",
+  "body": " Radian   A radian is a unit of measurement of angle size, and 1 radian is defined to be the angle that is made when the arc length for that angle on a circle of radius 1 is equal to 1. Check out this Desmos link and click the \"play\" button on the slider for a visual representation of this concept.    Standard Angles    A circle with common angles indicated in radians:   0     30     45     60     90     120     135     150     180     210     225     240     270     300     315     330          "
+},
+{
+  "id": "definitions-angles-7",
+  "level": "2",
+  "url": "sec-angles.html#definitions-angles-7",
+  "type": "Definition",
+  "number": "5.1.7",
+  "title": "Initial and Terminal sides.",
+  "body": " Initial and Terminal sides   An angle is created by imagining two rays originating from the same point: the initial side of the angle is stationary and the terminal side is rotated around until the desired angle is created. Angles measured from initial to terminal side in a counterclockwise direction are positive. Angles measured from initial to terminal side clockwise are negative.   "
+},
+{
+  "id": "definitions-angles-8",
+  "level": "2",
+  "url": "sec-angles.html#definitions-angles-8",
+  "type": "Definition",
+  "number": "5.1.8",
+  "title": "Standard Position.",
+  "body": " Standard Position   An angle is said to be in standard position if the initial and terminal sides of the angle meet at the origin and the initial side of the angle is along the positive x-axis.   "
+},
+{
+  "id": "definitions-angles-9",
+  "level": "2",
+  "url": "sec-angles.html#definitions-angles-9",
+  "type": "Definition",
+  "number": "5.1.9",
+  "title": "Coterminal.",
+  "body": " Coterminal   Two angles are coterminal if their initial and terminal sides align, but they differ by an integer number of full rotations. In standard position, this means that the two angles are “in the same place” are different by some integer multiple of 360 (or radians).   "
+},
+{
+  "id": "definitions-angles-10",
+  "level": "2",
+  "url": "sec-angles.html#definitions-angles-10",
+  "type": "Definition",
+  "number": "5.1.10",
+  "title": "Arc length.",
+  "body": " Arc length   An arc length is a distance along a part of the circumference of a circle. A formula for arc length is , where stands for arc length along a circle of radius , with an interior angle centered at the center of the circle.    Coterminal angles and    An angle drawn in standard position is indicated in two ways. Beginning with an initial side along the positive -axis and ending with a terminal side in the second quadrant, the angle in the counter- clockwise direction is labeled and the angle in the clockwise direction is labeled . The segment of the circumference of the circle between the initial and terminal sides of is the arc subtended by , and the length measure of that arc is the arc length for angle .      "
+},
+{
+  "id": "exit-angles-2",
+  "level": "2",
+  "url": "sec-angles.html#exit-angles-2",
+  "type": "Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Explain why -90 is not the same as 270 .   "
+},
+{
+  "id": "exit-angles-3",
+  "level": "2",
+  "url": "sec-angles.html#exit-angles-3",
+  "type": "Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Convert 34 29 24 to decimal degrees.   "
+},
+{
+  "id": "exit-angles-4",
+  "level": "2",
+  "url": "sec-angles.html#exit-angles-4",
+  "type": "Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Convert the angle 144 to radians. Write your answer in exact form as a fraction with in it.   "
+},
+{
+  "id": "exit-angles-5",
+  "level": "2",
+  "url": "sec-angles.html#exit-angles-5",
+  "type": "Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  How big of an arc length will an angle of radians create on a circle of radius 6? Draw this angle and arc length on the circle of radius 6 provided.    "
+},
+{
+  "id": "sec-angles-8-1",
+  "level": "2",
+  "url": "sec-angles.html#sec-angles-8-1",
+  "type": "Reading Question",
+  "number": "1",
+  "title": "",
+  "body": "  On a scale of 1 5, how are you feeling with the concepts related to the graphical behaviors of functions?   "
 },
 {
   "id": "sec-right-angle-trig",
